c6c566bb2fd4e3c51e75046a13ab53707c0ca23a
Lennon <1215197437@qq.com>
2023-12-21 15:49:24 +0800

    初始化

---
+++ src/views/Index/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="margin-top: 30px">
+    <GrowCard />
+  </div>
+</template>
+
+<script>
+import GrowCard from './components/GrowCard.vue';
+export default {
+  name: "Welcome",
+  components: {
+    GrowCard  // 局部注册子组件
+  },
+  mounted() {
+    var curTime = new Date();
+    if (window.localStorage.TokenExpire) {
+      var expiretime = new Date(Date.parse(window.localStorage.TokenExpire));
+      if (curTime >= expiretime) {
+        this.$router.push("/login");
+      }
+    } else {
+      this.$router.push("/login");
+    }
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Index/components/GrowCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+    1111
+  </div>
+</template>
+<script>
+export default {
+  name: "GrowCard",
+  data(){
+    return{
+        
+    }
+  }
+};
+</script>
